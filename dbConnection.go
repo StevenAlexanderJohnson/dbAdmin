68e48cc55fe41425479337f470a28842de239b94
Steven Johnson <steveajohn1@gmail.com>
2024-03-27 17:41:40 +0000

    added in code doc

--- dbConnection.go
+++ dbConnection.go
@@ -6,13 +6,10 @@ import (
 	_ "github.com/microsoft/go-mssqldb"
 )
 
+// Interface all Database structs (i.e. MongoDatabase) should fulfil.
 type Database interface {
 	Initialize() error
 	Disconnect() error
-}
-
-type DataResult interface {
-	UserPermissionResult
 }
 
 type UserPermissionResult struct {
@@ -21,6 +18,13 @@ type UserPermissionResult struct {
 	ObjectName     *string `json:"ObjectName"`
 }
 
+// This interface acts like a constraint for what structs can be used for the generic type T in QueryResult.
+// Any struct that is used to parse what's returned from queries should be added here.
+type DataResult interface {
+	UserPermissionResult
+}
+
+// Struct that will be returned from Go to the frontend.
 type QueryResult[T DataResult] struct {
 	Duration time.Duration `json:"Duration"`
 	Data     []T           `json:"Data"`
