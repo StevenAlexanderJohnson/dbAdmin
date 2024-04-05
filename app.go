2a2831165f08d84d8930efd9c90ae204cbfb8d84
Steven Johnson <steveajohn1@gmail.com>
2024-04-05 17:29:19 +0000

    Changed FindUserPermission to be in interface

--- app.go
+++ app.go
@@ -127,18 +127,7 @@ func (a *App) GetUserPermissions(databaseKey string, user string, target string)
 		return "", fmt.Errorf("%s has not been registered yet", databaseKey)
 	}
 
-	var err error
-	var queryResult QueryResult[UserPermissionResult]
-
-	switch v := db.(type) {
-	case *MongoDatabase:
-		queryResult, err = v.FindUserPermissions(user)
-	case *MsSqlDatabase:
-		queryResult, err = v.QueryUserPermissions(user, target)
-	default:
-		return "", fmt.Errorf("an error occurred while collecting user permissions")
-	}
-
+	queryResult, err := db.FindUserPermissions(user, target)
 	if err != nil {
 		return "", fmt.Errorf("an error occurred while collecting user permissions\n%s", err)
 	}
