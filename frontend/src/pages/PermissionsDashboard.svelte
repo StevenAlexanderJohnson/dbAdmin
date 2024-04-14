a78b90ef8923830b528863d6a8549807dcd9b469
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-04-14 16:48:05 -0400

    Adding permissions page. Modified how routes worked

---
+++ frontend/src/pages/PermissionsDashboard.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { link } from "svelte-spa-router";
+    import {location} from "svelte-spa-router";
+</script>
+
+<div class="h-full w-full flex flex-col">
+    <div class="flex-1 flex flex-col gap-20 p-10">
+        <a
+            use:link={{ href: `${$location}/users`, disabled: false }}
+            class="rounded-2xl bg-gradient-to-br from-accent to-text w-1/12 aspect-square flex justify-center items-center drop-shadow-md"
+        >
+            <span class="text-background text-center font-extrabold">Find Users</span>
+        </a>
+    </div>
+</div>
