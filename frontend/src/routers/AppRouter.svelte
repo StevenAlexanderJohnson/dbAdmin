d2098e8d4e13f792b922294b934b935978037b32
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-04-17 21:00:20 -0400

    forgot to add new file

---
+++ frontend/src/routers/AppRouter.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Home from "../pages/Home.svelte";
+    import Register from "../pages/Register.svelte";
+    import Router from "svelte-spa-router";
+    import PermissionsRouter from "../routers/PermissionsRouter.svelte";
+
+    const routes = {
+        "/": Home,
+        "/register": Register,
+        "/permissions": PermissionsRouter,
+        "/permissions/*": PermissionsRouter,
+    };
+</script>
+
+<Router {routes} />
