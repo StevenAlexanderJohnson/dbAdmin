4d75b8b078931d7b51a5da4105932960f911144a
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2024-05-08 20:32:36 -0400

    updated permission page (#44)

--- frontend/src/pages/PermissionsDashboard.svelte
+++ frontend/src/pages/PermissionsDashboard.svelte
@@ -1,15 +1,45 @@
 <script>
-    import { link } from "svelte-spa-router";
-    import {location} from "svelte-spa-router";
+    import { GetUserPermissions } from "../lib/wailsjs/go/main/App";
+    import { selectedConnection, selectedUser } from "../store";
+
+    let selectedConnectionValue = "";
+    selectedConnection.subscribe((value) => (selectedConnectionValue = value));
+    let selectedUserValue = "";
+    selectedUser.subscribe((value) => (selectedUserValue = value));
+
+    let selectedDatabaseValue = "";
+
+    let userPermissions = [];
+    $: GetUserPermissions(
+        selectedConnectionValue,
+        selectedUserValue,
+        selectedDatabaseValue,
+    )
+        .then((data) => (userPermissions = JSON.parse(data)["Data"]))
+        .catch((err) => console.error(err));
 </script>
 
-<div class="h-full w-full flex flex-col">
-    <div class="flex-1 flex flex-col gap-20 p-10">
-        <a
-            use:link={{ href: `${$location}/users`, disabled: false }}
-            class="rounded-2xl bg-gradient-to-br from-accent to-text w-1/12 aspect-square flex justify-center items-center drop-shadow-md"
-        >
-            <span class="text-background text-center font-extrabold">Find Users</span>
-        </a>
+<div class="h-full">
+    <div
+        class="mt-10 grid grid-cols-4 w-1/2 min-w-max
+               mx-auto border border-e-red-50
+               p-5"
+    >
+        <span>User</span>
+        <span>Role</span>
+        <span>Database</span>
+        <span>
+            <button type="button" class="underline underline-offset-4 hover:text-primary">
+                Grant Permssion
+            </button>
+        </span>
+        {#each userPermissions as permission}
+            <span>{permission.Name}</span>
+            <span>{permission.PermissionName}</span>
+            <span>{permission.ObjectName}</span>
+            <div>
+                <button class="underline underline-offset-4 hover:text-primary">Remove</button>
+            </div>
+        {/each}
     </div>
 </div>
